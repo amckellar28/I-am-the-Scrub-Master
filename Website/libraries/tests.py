958465bcb34841a1c63a331e31955ad4311edb09
Alice McKellar <alice.mckellar@outlook.com>
2017-10-22 13:16:56 +1000

    299

--- Website/libraries/tests.py
+++ Website/libraries/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+from libraries.models import Library
+
+class librariesModelTest(TestCase):
+
+##    @classmethod
+##    def setUpTestData(cls):
+##        #Set up non-modified objects used by all test methods
+##        Library.objects.create(first_name='Big', last_name='Bob')
+
+
+
+    @classmethod
+    def setUpTestData(cls):
+        #Set up non-modified objects used by all test methods
+        Library.objects.create(title='library', address='13', number='0456',
+                               emailaddress='library@website.com', home_page='www.website.com',
+                               description='items')
+
+    def test_title_label(self):
+        item = Library.objects.get(id=1)
+        field_label = item.title
+        self.assertEquals(field_label,'library')
+        
+    def test_address_label(self):
+        item = Library.objects.get(id=1)
+        field_label = item.address
+        self.assertEquals(field_label,'13')
+
+    def test_number_label(self):
+        item = Library.objects.get(id=1)
+        field_label = item.number
+        self.assertEquals(field_label,'0456')
+
+    def test_emailaddress_label(self):
+        item = Library.objects.get(id=1)
+        field_label = item.emailaddress
+        self.assertEquals(field_label,'library@website.com')
+
